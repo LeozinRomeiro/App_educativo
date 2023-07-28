a149eaeb107e2126372a0e28f458c5a5e15e881a
Aluno <Aluno@SNCTM202LABF02>
2023-07-27 22:30:29 -0300

    Adicionar arquivos de projeto.

---
+++ App_educativo/App_educativo/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_educativo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void botao_cores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page_cores());
+        }
+
+        public void botao_familia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page_familia());
+        }
+
+        public void botao_numeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page_numeros());
+        }
+
+        private void botao_sair_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
---
+++ App_educativo/App_educativo/Page_familia.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_educativo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_familia : ContentPage
+    {
+        public Page_familia()
+        {
+            InitializeComponent();
+        }
+        private void botao_sair_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+    }
+}
